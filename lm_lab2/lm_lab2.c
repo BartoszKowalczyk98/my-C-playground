bceeae884c8eca1da5b5ef0b982edd1333ec8906
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2021-05-12 15:32:12 +0200

    sending this w chlere

--- lm_lab2/lm_lab2.c
+++ lm_lab2/lm_lab2.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include <string.h>
 
 
 int main(int argc, char const *argv[])
@@ -23,15 +23,26 @@ int main(int argc, char const *argv[])
         {{}   ,{}   ,{}   ,{}   ,{}    ,{}    ,{16}},
         {{16} ,{16} ,{16} ,{16} ,{16}  ,{16}  ,{16}}
     };
-    int tablica_stanow[100][100]= {0};
+    // tablica odpowiadająca za przechowywanie listy aktualnych stanów
+    int tablica_stanow[100]= {0};
+    // pomocnicza zmienna przechowująca iterator ile jest aktualnie dzialajacych stanow do analaizy
     int ile_aktywnych_stanow = 0;
+
+    // tablica char potrzebna do wyświetlania przejsc
+    char historia_przejsc[100][300];
+    for (int j = 0; j < 100; j++)
+    {
+        strcpy(historia_przejsc[j],"");
+    }
+    char slowo[100];
+    
 
     // wczytywanie pliku
     printf("Podaj nazwę pliku: ");
     char nazwa_pliku[20];
     scanf("%s",&nazwa_pliku);
     
-    FILE *f = fopen("testingnfa.txt","r");
+    FILE *f = fopen(nazwa_pliku,"r");
     if(f == NULL){
         printf("Blad podczas wczytywania pliku!\n");
         return -1;
@@ -42,79 +53,142 @@ int main(int argc, char const *argv[])
     printf("rozpoczynam wczytywanie z pliku: \n");
     while((ch = fgetc(f)) != EOF){
         ile_aktywnych_stanow++;
+
+        // switch do tworzenia histori przejsc i zapamietywania slowa w celu pozniejszego wyprintowania
+        switch (ch)
+        {
+        case '0':
+            strcat(slowo,"0");
+            strcat(historia_przejsc[0]," -> 0 -> q0");
+            break;
+        case '1':
+            strcat(slowo,"1");
+            strcat(historia_przejsc[0]," -> 1 -> q0");
+            break;
+        case '2':
+            strcat(slowo,"2");
+            strcat(historia_przejsc[0]," -> 2 -> q0");
+            break;
+        case '3':
+            strcat(slowo,"3");
+            strcat(historia_przejsc[0]," -> 3 -> q0");
+            break;
+        case 'a':
+            strcat(slowo,"a");
+            strcat(historia_przejsc[0]," -> a -> q0");
+            break;
+        case 'b':
+            strcat(slowo,"b");
+            strcat(historia_przejsc[0]," -> b -> q0");
+            break;
+        case 'c':
+            strcat(slowo,"c");
+            strcat(historia_przejsc[0]," -> c -> q0");
+            break;
+
+        default:
+            break;
+        }
         for (int i = 0; i < ile_aktywnych_stanow; i++)
         {
             // if do rozpoznawania znaku na wejsciu i przejściu do odpowiedniego dla niego stanu 
             if(ch == '0'){
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][0][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][0][0];
+                if(tablica_przejsc[tablica_stanow[i]][0][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][0][0];
+                    char temp[10];
+                    sprintf(temp," -> 0 -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == '1')
             {
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][1][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][1][0];
+                 if(tablica_przejsc[tablica_stanow[i]][1][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][1][0];
+                    char temp[10];
+                    sprintf(temp," -> 1 -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == '2')
             {
-               if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][2][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][2][0];
+               if(tablica_przejsc[tablica_stanow[i]][2][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][2][0];
+                    char temp[10];
+                    sprintf(temp," -> 2 -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == '3')
             {
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][3][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][3][0];
+                if(tablica_przejsc[tablica_stanow[i]][3][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][3][0];
+                    char temp[10];
+                    sprintf(temp," -> 3 -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == 'a')
             {
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][4][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][4][0];
+                if(tablica_przejsc[tablica_stanow[i]][4][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][4][0];
+                    char temp[10];
+                    sprintf(temp," -> a -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == 'b')
             {
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][5][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][5][0];
+               if(tablica_przejsc[tablica_stanow[i]][5][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][5][0];
+                    char temp[10];
+                    sprintf(temp," -> b -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch == 'c')
             {
-                if(tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][6][0]!=0){
-                    tablica_stanow[i][ile_aktywnych_stanow] = tablica_przejsc[tablica_stanow[i][ile_aktywnych_stanow]][6][0];
+               if(tablica_przejsc[tablica_stanow[i]][6][0]!=0){
+                    tablica_stanow[i] = tablica_przejsc[tablica_stanow[i]][6][0];
+                    char temp[10];
+                    sprintf(temp," -> c -> q%d",tablica_stanow[i]);
+                    strcat(historia_przejsc[i+1],temp);
                 }
             }
             else if (ch=='#')
             {
                 // printowanie tablicy przejsc w ładny sposób
-
+                int czy_zakonczyl_w_stanie_akceptujacym = 0;
                 for (int j = 0; j < ile_aktywnych_stanow; j++)
                 {
-                    
                     printf("q0");
-                    for (int k = 0; k < ile_aktywnych_stanow; k++)
+                    if(tablica_stanow[j]!=0)
                     {
-                        /* code */
-                        if(tablica_stanow[j][k]!=0)
+                        if (czy_zakonczyl_w_stanie_akceptujacym==0&&tablica_stanow[j]==9)
                         {
-                            printf(" -> q%d",  tablica_stanow[j][k]);
+                            czy_zakonczyl_w_stanie_akceptujacym=1;
                         }
-                        
-                    }
-                    printf("\n");
-                    
+                        else if (czy_zakonczyl_w_stanie_akceptujacym==0&&tablica_stanow[j]==16)
+                        {
+                            czy_zakonczyl_w_stanie_akceptujacym=2;
+                        }
+                    }   
+                    printf("%s\n",  historia_przejsc[j]);                   
                 }
+                if(czy_zakonczyl_w_stanie_akceptujacym ==1)
+                    printf("Dla slowa %s automat osiagnal stan akceptujacy dla potrojenia cyfr q9\n", slowo);
+                else if (czy_zakonczyl_w_stanie_akceptujacym==2)
+                    printf("Dla slowa %s automat osiagnal stan akceptujacy dla potrojenia liter q16\n", slowo);
+                else
+                    printf("Dla slowa %s automat nie osiagnal stanu akceptujacego\n", slowo);
+                strcpy(slowo,"");
                 printf("\n");
 
+                // reset potrzebnych tablic przed nastepnym slowem 
                 ile_aktywnych_stanow=0;
                 for (int j = 0; j < 100; j++)
                 {
-                    for (int k = 0; k < 100; k++)
-                    {
-                        tablica_stanow[j][k]=0;
-                    }
+                    tablica_stanow[j]=0;
+                    strcpy(historia_przejsc[j],"");
                 }
             }
             else
@@ -124,9 +198,6 @@ int main(int argc, char const *argv[])
                 return -1;
             }
         }
-
-        
-
     }
     fclose(f);
     return 0;
