97cd91540d3ad91505b7fd49ca26f7295798e5c3
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2021-06-08 17:46:12 +0200

    initial commit for lab 5 and some small changes to lab4

---
+++ lm_lab5/lm_lab5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+   
+    // wprowadzanie zmiennych przez użytkownika
+    printf("Podaj wyrazenie do sprawdzenia: ");
+    char wyrazenie[300];
+    scanf("%s", wyrazenie);
+    
+   
+}
